da7693e6e55bb1199deda8a52505050f206a75cb
Ashwini Khandagale <ashwini.khandagale@virgoinnovation.com>
2020-02-24 10:51:47 +0530

    minor changes added

--- src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
+++ src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
@@ -63,7 +63,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         
         Log.d(TAG, "\tNotification Data: " + data.toString());
          // FCMPlugin.sendPushPayload( data );
-         sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+         sendNotification(remoteMessage.getData().getTitle(), remoteMessage.getData().getBody());
     // [END receive_message]
     }
     /**
